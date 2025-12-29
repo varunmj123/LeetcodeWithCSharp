f998f8804b8390d3c7c4f0dc10d8c4779f7cb368
Varun Jha <61578176+varunmj123@users.noreply.github.com>
2025-12-30 03:12:18 +0800

    Time: 1 ms (99.91%) | Memory: 59.4 MB (31.62%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cs
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cs
@@ -1,16 +1,17 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
-        int min = prices[0];
+        int min = 0;
         int maxProfit = 0;
         for(int i = 1; i < prices.Length; i++){
-            int currentProfit = prices[i] - min;
-            if(currentProfit > maxProfit){
-                maxProfit = currentProfit;
+            if(prices[min] > prices[i]){
+                min = i;
             }
-            if(prices[i] < min){
-                min = prices[i];
+            else{
+                int currentProfit = prices[i] - prices[min];
+                if(currentProfit > maxProfit){
+                    maxProfit = currentProfit; 
+                }
             }
-            
         }
         return maxProfit;
     }
