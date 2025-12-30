f8db3da42a0941fd547755506d695a6100e34d98
Varun Jha <61578176+varunmj123@users.noreply.github.com>
2025-12-31 01:22:23 +0800

    Time: 1 ms (6.76%) | Memory: 53 MB (23.74%) - LeetSync

--- 792-binary-search/binary-search.cs
+++ 792-binary-search/binary-search.cs
@@ -1,19 +1,20 @@
 public class Solution {
     public int Search(int[] nums, int target) {
         int left = 0;
-        int right = nums.Length -1; 
+        int right = nums.Length -1;
+
         while(left <= right){
-            int middle = (left + right)/ 2;
-            if(nums[middle] > target){
-                right = middle - 1;
+            int mid = (left + right)/2;
+            if(nums[mid] == target){
+                return mid;
             }
-            else if(nums[middle] < target){
-                left = middle + 1;
+            else if(nums[mid] > target){
+                right = mid - 1;
             }
-            else{
-                return middle; 
+            else if(nums[mid] < target){
+                left = left + 1;
             }
         }
-        return -1; 
+        return -1;
     }
 }
