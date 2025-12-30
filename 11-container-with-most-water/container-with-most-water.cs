47fb30ed08b147444a875086def77b9851802d86
Varun Jha <61578176+varunmj123@users.noreply.github.com>
2025-12-31 02:05:33 +0800

    Time: 1 ms (99.54%) | Memory: 63.8 MB (11.50%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cs
+++ 11-container-with-most-water/container-with-most-water.cs
@@ -1,57 +1,21 @@
 public class Solution {
-    public int MaxArea(int[] heights) {
+    public int MaxArea(int[] height) {
         int left = 0;
-        int right = heights.Length - 1;
-        int maxArea = 0;
+        int right = height.Length -1 ;
+        int maxVolume = 0;
         while(left < right){
-            int maxAllowedWater = Math.Min(heights[left], heights[right]);
-            int totalDistance = right - left;
-            int currentArea = maxAllowedWater * totalDistance;
-            if(currentArea > maxArea){
-                maxArea = currentArea;
+            int minHeight = Math.Min(height[left], height[right]);
+            int currentVolume = (right - left) * minHeight;
+            if(currentVolume > maxVolume){
+                maxVolume = currentVolume;
             }
-            // If the left is bigger move the right pointer back
-            if(heights[left] > heights[right]){
+            if(height[left] > height[right]){
                 right--;
-                continue;
             }
-            // If the right is bigger move the left pointer front
-            else if(heights[right] > heights[left]){
-                left++;
-                continue;
-            }
-            // If they are the same it does not matter and we can move either
             else{
                 left++;
             }
-
         }
-        return maxArea;
+        return maxVolume;
     }
 }
-
-/* Brute force 
-public class Solution {
-    public int MaxArea(int[] heights) {
-        int maxArea = 0;
-        int left = 0;
-        while(left < heights.Length){
-            int right = left + 1; 
-            while(right < heights.Length){
-                int maxAllowedWater = Math.Min(heights[left], heights[right]);
-                int totalDistance = right - left;
-                int currentArea = maxAllowedWater * totalDistance; 
-                if(currentArea > maxArea){
-                    maxArea = currentArea;
-                }
-                right++;
-                continue;
-            }
-            left++;
-            continue;
-        }
-        return maxArea;
-    }
-}
-
-*/
