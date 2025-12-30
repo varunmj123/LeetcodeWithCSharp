4780dbe2e5069949724dcf76b2cd6db195853afb
Varun Jha <61578176+varunmj123@users.noreply.github.com>
2025-12-30 23:58:27 +0800

    Time: 0 ms (100.00%) | Memory: 46.9 MB (91.51%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cs
+++ 88-merge-sorted-array/merge-sorted-array.cs
@@ -1,18 +1,18 @@
 public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
-        // Right counter will start from the back of nums2
-        int right = n - 1;
-        // Left counter will start from the middle of nums1
-        int left = m - 1;
-        // i counter will start from the back of nums1
-        for (int i = m + n - 1; i >= 0; i--) {
-            if (right < 0) break;
-
+        int left = m -1; 
+        int right = n -1;
+        //int pointer = m + n -1 ;
+        
+        for(int pointer = m + n -1; pointer >= 0; pointer--){
+            if(right < 0){
+                break;
+            }
             if (left >= 0 && nums1[left] > nums2[right]) {
-                nums1[i] = nums1[left];
+                nums1[pointer] = nums1[left];
                 left--;
             } else {
-                nums1[i] = nums2[right];
+                nums1[pointer] = nums2[right];
                 right--;
             }
         }
