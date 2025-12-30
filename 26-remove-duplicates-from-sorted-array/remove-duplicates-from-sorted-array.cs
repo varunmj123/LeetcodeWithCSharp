ca542acb42938629d250a4d6d510008d69c87771
Varun Jha <61578176+varunmj123@users.noreply.github.com>
2025-12-31 03:08:30 +0800

    Time: 0 ms (100.00%) | Memory: 51.4 MB (26.83%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cs
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cs
@@ -1,15 +1,28 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
         int pointer = 0;
-        var seenNumber = new Dictionary<int , int>();
-
-        for(int i=0; i < nums.Length; i++){
-            if(!seenNumber.ContainsKey(nums[i])){
-                nums[pointer] = nums[i];
+        for(int i =0; i < nums.Length; i++){
+            if(nums[pointer] != nums[i]){
+                nums[pointer + 1] = nums[i];
                 pointer++;
             }
-            seenNumber[nums[i]] = i;
         }
-        return pointer;
+        return pointer + 1;
     }
 }
+
+//public class Solution {
+//     public int RemoveDuplicates(int[] nums) {
+//         int pointer = 0;
+//         var seenNumber = new Dictionary<int , int>();
+
+//         for(int i=0; i < nums.Length; i++){
+//             if(!seenNumber.ContainsKey(nums[i])){
+//                 nums[pointer] = nums[i];
+//                 pointer++;
+//             }
+//             seenNumber[nums[i]] = i;
+//         }
+//         return pointer;
+//     }
+// }
