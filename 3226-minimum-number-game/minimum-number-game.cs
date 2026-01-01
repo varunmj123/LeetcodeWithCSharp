e44c153533f8f18976c03026b67d765106058996
Varun Jha <61578176+varunmj123@users.noreply.github.com>
2026-01-02 02:09:35 +0800

    Time: 3 ms (100.00%) | Memory: 49.9 MB (32.39%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public int[] NumberGame(int[] nums) {
+ 
+        int[] output = new int[nums.Length];
+        int numberOfTurns = nums.Length/2;
+        int position = 0;
+        while(numberOfTurns > 0){
+            int aliceMin = int.MaxValue;
+            int aliceMinIndex = 0;
+             for(int i =0; i < nums.Length; i++){
+                if(nums[i] < aliceMin){
+                    aliceMin = nums[i];
+                    aliceMinIndex = i;
+                }
+             }
+             nums[aliceMinIndex] = int.MaxValue;
+            int bobMin = int.MaxValue;
+            int bobMinIndex = 0;
+            for(int i = 0; i < nums.Length; i++){
+                if(nums[i] < bobMin){
+                    bobMin = nums[i];
+                    bobMinIndex = i;
+                }
+            }
+            nums[bobMinIndex] = int.MaxValue;
+            output[position] = bobMin;
+            position++;
+            output[position] = aliceMin;
+            position++;
+            numberOfTurns--;
+        }
+        return output;
+    }
+}
