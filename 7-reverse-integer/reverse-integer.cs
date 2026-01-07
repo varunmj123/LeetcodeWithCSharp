9cc1f64ab95bbadc9e8ff079f04657aa7014c00d
Varun Jha <61578176+varunmj123@users.noreply.github.com>
2026-01-08 01:57:18 +0800

    Time: 33 ms (11.18%) | Memory: 29.5 MB (31.36%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public int Reverse(int x) {
+        if(x > int.MaxValue || x < int.MinValue){
+            return 0;
+        }
+        bool isNegative = false;
+        if(x < 0){
+            isNegative = true;
+        }
+        string xString = x.ToString();
+        string output = "";
+        if(isNegative){
+            output += "-";
+            for(int i = xString.Length -1  ; i > 0; i--){
+                output += xString[i];
+        }
+        }
+        else{
+            for(int i = xString.Length - 1; i >= 0; i--){
+                output += xString[i];
+        }
+        }
+        bool success = int.TryParse(output, out int result);
+        if(success){
+            return int.Parse(output);
+        }
+        else{
+            return 0;
+        }
+   
+        
+        
+    }
+}
